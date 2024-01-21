0f6069ff5f365a278d34eb1abe69b7c98b1ba9a5
Thanasak1412 <Thanasak.sr60@snru.ac.th>
2024-01-21 12:16:45 +0700

    Update response user data to exclude sensitive data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,12 +10,15 @@ import {
   Post,
 } from '@nestjs/common';
 
+import { Serialize } from '../interceptor/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
